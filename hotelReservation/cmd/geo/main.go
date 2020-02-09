bf3f66a483b56fd675f4f95556d8b0c1bc7c3720
Fatima Tahir <33696029+Fatahir@users.noreply.github.com>
2020-02-09 13:23:58 +0500

    Update main.go

--- hotelReservation/cmd/geo/main.go
+++ hotelReservation/cmd/geo/main.go
@@ -9,9 +9,9 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/harlow/go-micro-services/registry"
-	"github.com/harlow/go-micro-services/services/geo"
-	"github.com/harlow/go-micro-services/tracing"
+	"github.com/Fatahir/ds/tree/master/hotelReservation/registry"
+	"github.com/Fatahir/ds/tree/master/hotelReservation/services/geo"
+	"github.com/Fatahir/ds/tree/master/hotelReservation/tracing"
 )
 
 func main() {
